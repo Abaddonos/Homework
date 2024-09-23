1d76ebe803f7248c79a0153e60feb7103d5d7ac8
Aboba <dzhiobbos1512017@gmail.com>
2024-09-24 00:40:00 +0300

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,7 @@
+Number_of_completed = 12
+Number_of_hours_spent = 1.5
+Course_Title = 'Python'
+average_execution_time = Number_of_hours_spent / Number_of_completed
+print('Курс:', Course_Title + ', всего задач:' + str(Number_of_completed) + ', затрачено часов:'
+      + str(Number_of_hours_spent) + ', среднее время выполнения:' + str(average_execution_time))
+# команду str использовал, чтобы избавиться от пробелов :)
