531c29747ada343e5d0df7af7c21a54b0991edad
Fobbos <dzhiobbos1512017@gmail.comgit config --global user.name$ git config --global user.name$ git config --global user.namegit config --global user.name Abbos]]>
2024-09-24 23:41:33 +0300

    Save

--- HomeWork.py
+++ HomeWork.py
@@ -1,17 +1,22 @@
 print("Hello Urban University")
 #  1st program
 print(9 ** 0.5 * 5)
+
 #  2nd program
 print(9.99 > 9.98 and 1000 != 1000.1)
+
 #  3nd program
 print(1234 % 100 + 5678 // 100)
+
 #  4nd program
 print(13.42 * 100 % 100 == int(42.13) and int(13.42) == 42.13 * 100 % 100 or 22 != 22, 'Куку')
 
 print('Это все старая версия, далее новая начиная с 3ей задачи')
 
 print('Hello again')
+
 #  3nd program
-print(2 + 2 * 2 == (2 + 2) * 2)
+print(2 + 2 * 2 == (2 + 2) * 2)            # Задача получить False в итоге
+
 #  4nd program
-print(int(123.456 * 10) % 10)
+print(int(float('123.456') * 10) % 10)
