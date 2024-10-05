ec9fe38f2e4776253f523efed953090d88ae7bea
Fobbos <dzhiobbos1512017@gmail.comgit config --global user.name$ git config --global user.name$ git config --global user.namegit config --global user.name Abbos]]>
2024-10-05 21:26:40 +0300

    Save

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('Опишите погоду на улице в 3 словах. ')    # Допустим: Холодно,     пасмурно,     темно 222
+print(len(my_string))
+print(str(my_string .upper()))
+print(str(my_string .lower()))
+print(str(my_string .replace(' ', '')))
+print(my_string[0])
+print(my_string[-1])
