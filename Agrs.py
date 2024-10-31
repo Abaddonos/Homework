139f4a9747c22b23d41bd994163f6a54c72b9bc3
Fobbos <dzhiobbos1512017@gmail.comgit config --global user.name$ git config --global user.name$ git config --global user.namegit config --global user.name Abbos]]>
2024-10-31 20:06:21 +0300

    Save

--- Agrs.py
+++ Agrs.py
@@ -1,13 +1,15 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     while len(other_words) > 0:
+        #other_words = str(other_words.lower)
+        root_word = str(root_word.lower)
         for i in root_word:
             for j in other_words:
-                if i in j or j in i:
+                if i in j:
                     same_words.append(j)
-                else:
                     continue
-                return same_words
+                else:
+                    return same_words
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
 print(result1)
