30d762aaf792554d70a12fdb511249db647c40f6
Fobbos <dzhiobbos1512017@gmail.comgit config --global user.name$ git config --global user.name$ git config --global user.namegit config --global user.name Abbos]]>
2024-12-19 17:37:39 +0300

    Save

--- openfile.py
+++ openfile.py
@@ -9,13 +9,13 @@ class Product:
 class Shop:
     __file_name = 'products.txt'
     def get_products(self):
-        file = open(self.__file_name, 'r+')
+        file = open(self.__file_name, 'r')
         prod_str = file.read()
         file.close()
         return prod_str
     def add(self, *products):
         for i in products:
-            if self.get_products().find(f'{i.name},') == 1:
+            if self.get_products().find(f'{i.name},') == -1:
                 file = open(self.__file_name, 'a')
                 file.write(f'{i}\n')
                 file.close()
